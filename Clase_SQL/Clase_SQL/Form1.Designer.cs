50c16718c8dea04079ea16e5b2788411223483fc
Demian Boullon <dboullon@q4tech.com>
2017-10-31 16:24:05 -0300

    Clase SQL

---
+++ Clase_SQL/Clase_SQL/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Clase_SQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.btnProv = new System.Windows.Forms.Button();
+            this.btnLoc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(33, 58);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(262, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextChanged += new System.EventHandler(this.comboBox1_TextChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(33, 107);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(262, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // btnProv
+            // 
+            this.btnProv.Location = new System.Drawing.Point(33, 7);
+            this.btnProv.Name = "btnProv";
+            this.btnProv.Size = new System.Drawing.Size(47, 23);
+            this.btnProv.TabIndex = 2;
+            this.btnProv.Text = "Load";
+            this.btnProv.UseVisualStyleBackColor = true;
+            this.btnProv.Click += new System.EventHandler(this.btnProv_Click);
+            // 
+            // btnLoc
+            // 
+            this.btnLoc.Location = new System.Drawing.Point(314, 58);
+            this.btnLoc.Name = "btnLoc";
+            this.btnLoc.Size = new System.Drawing.Size(62, 70);
+            this.btnLoc.TabIndex = 3;
+            this.btnLoc.Text = "Loc.";
+            this.btnLoc.UseVisualStyleBackColor = true;
+            this.btnLoc.Click += new System.EventHandler(this.btnLoc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 273);
+            this.Controls.Add(this.btnLoc);
+            this.Controls.Add(this.btnProv);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button btnProv;
+        private System.Windows.Forms.Button btnLoc;
+    }
+}
+
---
+++ Clase_SQL/Clase_SQL/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Clase_SQL
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionStr = "Data Source=.\\sqlexpress;Initial Catalog=Argentina;Integrated Security=True";
+
+            SqlConnection sqlConnection = new SqlConnection(connectionStr);
+
+            SqlCommand comando = new SqlCommand("Select * from Provincia", sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                SqlDataReader sqlDataReader;
+                sqlDataReader = comando.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    this.comboBox1.Items.Add(sqlDataReader["Descripcion"]);
+                }
+                sqlConnection.Close();
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        private void btnProv_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLoc_Click(object sender, EventArgs e)
+        {
+
+          
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string connectionStr5 = "Data Source=.\\sqlexpress;Initial Catalog=Argentina;Integrated Security=True";
+
+            SqlConnection sqlConnection2 = new SqlConnection(connectionStr5);
+
+            SqlCommand comando2 = new SqlCommand("select * from Localidad  L inner join Provincia P on P.id = L.idProvincia where p.descripcion = '" + this.comboBox1.Text + "'",sqlConnection2);
+            
+            try
+            {
+                sqlConnection2.Open();
+                SqlDataReader sqlDataReader;
+                sqlDataReader = comando2.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    this.comboBox2.Items.Add(sqlDataReader["Nombre"]);
+                }
+                
+            }
+            catch
+            {
+
+            }
+            finally
+            {
+                sqlConnection2.Close();
+            }
+        }
+    }
+}
