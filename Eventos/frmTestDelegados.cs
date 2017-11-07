aca24bd26ad5bc67f23c76bb6700df13fa9deef7
demian44 <demi-44@hotmail.com>
2017-11-06 22:07:06 -0300

    Eventos

---
+++ Eventos/frmTestDelegados.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eventos
+{
+    public partial class frmTestDelegados : Form
+    {
+        public frmTestDelegados()
+        {
+            InitializeComponent();
+        }
+        
+        
+
+        private void frmTestDelegados_Load(object sender, EventArgs e)
+        {
+            
+        }
+       
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
